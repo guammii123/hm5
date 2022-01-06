be19373a76b463ac05b19702677a2ac464c1593b
User2 <marcenkoadel651@gmail.com>
2022-01-07 01:41:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[] args ){
+        Boss nike = new Boss(1234,766,"the zachita");
+        System.out.println(" Boss.health " +  nike.getHealth() + " " + nike.getDamage() + " " + nike.getBossDeafanse ());
+    }
+}
